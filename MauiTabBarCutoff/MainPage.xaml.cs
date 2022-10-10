2d1d12a073f6c670952e565dc1c4ec120e09449b
Julian Ewers <ewerspej@tcd.ie>
2022-10-10 18:26:29 +0200

    added screenshots and fixed formatting

--- MauiTabBarCutoff/MainPage.xaml.cs
+++ MauiTabBarCutoff/MainPage.xaml.cs
@@ -3,14 +3,15 @@
 public partial class MainPage : ContentPage
 {
     public MainPage()
-	{
-		InitializeComponent();
-	}
+    {
+        InitializeComponent();
+    }
 
     protected override void OnSizeAllocated(double width, double height)
     {
         base.OnSizeAllocated(width, height);
 
+        //setting this to true, then false and true again will lead to the cut-off
         Shell.SetTabBarIsVisible(this, height > width);
     }
 }
